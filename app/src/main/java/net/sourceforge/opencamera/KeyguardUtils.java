9ea67ce6de23a5fe4a35d3f35337251f18d702e8
Mark Harman <mark.harman.apps@gmail.com>
2024-04-28 16:47:33 +0100

    Updates for last release.

--- app/src/main/java/net/sourceforge/opencamera/KeyguardUtils.java
+++ app/src/main/java/net/sourceforge/opencamera/KeyguardUtils.java
@@ -5,8 +5,10 @@ import static android.content.Context.KEYGUARD_SERVICE;
 import android.app.Activity;
 import android.app.KeyguardManager;
 import android.os.Build;
+import android.util.Log;
 
 public class KeyguardUtils {
+    private static final String TAG = "KeyguardUtils";
 
     public static void requireKeyguard(Activity activity, Runnable callback) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -18,7 +20,11 @@ public class KeyguardUtils {
             keyguardManager.requestDismissKeyguard(activity, new KeyguardManager.KeyguardDismissCallback() {
                 @Override
                 public void onDismissSucceeded() {
+                    if( MyDebug.LOG )
+                        Log.d(TAG, "onDismissSucceeded");
                     callback.run();
+                    if( MyDebug.LOG )
+                        Log.d(TAG, "onDismissSucceeded: after callback run");
                 }
             });
         } else {
